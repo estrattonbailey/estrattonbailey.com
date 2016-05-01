994608f02291c16bc383c4f2057f188c59c84c28
estrattonbailey <ericstrattonbailey@gmail.com>
2016-05-01 16:03:29 -0400

    Update domain name

--- site.config.js
+++ site.config.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 
+var domain = process.env.PRODUCTION === 'true' 
+      ? 'https://estrattonbailey.herokuapp.com' 
+      : 'http://localhost:5000',
+
 var config = {
   meta: {
     version: '0.0.1',
     title: 'Eric Bailey',
-    url: process.env.PRODUCTION === 'true' ? https://estrattonbailey.herokuapp.com : http://localhost:5000/',
+    url: domain,
     author: '@estrattonbailey',
     description: 'Designer, developer.',
     keywords: 'developer',
