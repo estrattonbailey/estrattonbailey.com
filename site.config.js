b1da2cd02ce7bf9a4c701e6f2fb3bfe4b4f93643
estrattonbailey <ericstrattonbailey@gmail.com>
2016-05-01 16:00:20 -0400

    Update config vars for Contentful

--- site.config.js
+++ site.config.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 var config = {
   meta: {
     version: '0.0.1',
     title: 'Eric Bailey',
-    url: 'http://localhost:5000/',
+    url: process.env.PRODUCTION === 'true' ? https://estrattonbailey.herokuapp.com : http://localhost:5000/',
     author: '@estrattonbailey',
     description: 'Designer, developer.',
     keywords: 'developer',
